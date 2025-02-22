a712682aedf56fc79f1f0c2255bc65448de7e003
Sakshi Singhania <122166462+geekylearner-03@users.noreply.github.com>
2025-02-22 10:44:42 +0530

    Time: 8 ms (59.94%) | Memory: 11.4 MB (62.36%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,31 +3,37 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char, int> mpp;
         for (char c : t) {
-            mpp[c]++;
+            mpp[c]++; // Initialize the map with characters from t
         }
 
         int l = 0, r = 0, minLen = INT_MAX, sIndex = -1, cnt = 0, n = s.size(), m = t.size();
 
         while (r < n) {
+            // If the current character is in t, increment the count
             if (mpp[s[r]] > 0) {
                 cnt++;
             }
-            mpp[s[r]]--;
-            r++;
+            mpp[s[r]]--; // Decrement the count in the map
+            r++; // Move the right pointer
 
+            // When all characters in t are matched
             while (cnt == m) {
+                // Update the minimum window if the current window is smaller
                 if (r - l < minLen) {
                     minLen = r - l;
                     sIndex = l;
                 }
+
+                // Move the left pointer to try to find a smaller window
                 mpp[s[l]]++;
                 if (mpp[s[l]] > 0) {
-                    cnt--;
+                    cnt--; // If the character was part of t, decrement the count
                 }
                 l++;
             }
         }
 
+        // Return the smallest window or an empty string if no window exists
         return sIndex == -1 ? "" : s.substr(sIndex, minLen);
     }
 };
