18b722be6377a3d78afd41ef5e8d4a7f9cf35ed4
Sakshi Singhania <122166462+geekylearner-03@users.noreply.github.com>
2025-03-25 15:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (79.38%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-     int m = matrix.size();
-     int n = matrix[0].size();
-     vector<int>row(m,0);
-     vector<int>column(n,0);
-
-     for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
+     int n = matrix.size();
+     int m = matrix[0].size();
+     int col0 = 1;
+     for(int i=0;i<n;i++){
+        for(int j=0; j<m; j++){
             if(matrix[i][j] == 0){
-                row[i] = 1;
-                column[j] = 1;
+                matrix[i][0] = 0;
+                if(j!=0){
+                    matrix[0][j] = 0;
+                }
+                else col0 = 0;
             }
         }
      }
-     for(int i=0; i<m ;i++){
-        for(int j=0; j<n; j++){
-            if(row[i] || column[j]){
-                matrix[i][j] = 0;
+     for(int i=1; i<n;i++){
+        for(int j=1; j<m; j++){
+            if(matrix[i][j] != 0){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
             }
+        }
+     }
+     if(matrix[0][0] == 0){
+        for(int j=0;j<m;j++){
+            matrix[0][j] = 0;
+        }
+     }
+     if(col0 == 0){
+        for(int i=0; i<n; i++){
+            matrix[i][0] = 0;
         }
      }
     }
 };
 
-//samaj aa rha hai na?? yes
-//theek hai kro samaj kr haa 
-//vector lo aur define kro haa dp lagega jaha tk nhi
-// wait maine yeh q kiya tha striver se ...dekh ke batati hu ..usme approch bs yaad nai aaraha ahi 
-// isme in place algo lagega greedy approach k saath 
-// tum k hint de rha hu tumhe mujhe mile gaya approach 
-// kro abb hogaya run kro phirse kro run abb a
