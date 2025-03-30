316bb62977736fd8e5e94fc6e651175dcb602735
Sakshi Singhania <122166462+geekylearner-03@users.noreply.github.com>
2025-03-30 22:45:03 +0530

    Time: 183 ms (13.80%) | Memory: 53.7 MB (8.51%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            for(int j=i+1; j<n; j++){
-                if(j != (i+1) && nums[j]==nums[j-1]) continue;
-                int k= j+1;
-                int l = n-1;
-                while(k<l){
-                    long long sum = nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if(sum == target){
-                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
-                        ans.push_back(temp);
-                        k++; l--;
-                        while(k<l && nums[k] == nums[k-1]) k++;
-                        while(k<l && nums[l] == nums[l+1]) l--;
-                    }
-                    else if(sum<target) k++;
-                    else l--;
-                }
+       int n = nums.size();
+       sort(nums.begin(), nums.end());
+       set<vector<int>>set;
+       vector<vector<int>>output;
+       for(int i=0; i<n-3; i++){
+        for(int j=i+1; j<n-2; j++){
+         long long newTarget = (long long)target - (long long)nums[i] - (long long)nums[j];
+         int low = j+1;
+         int high = n-1;
+         while(low<high){
+            if(nums[low] + nums[high] < newTarget){
+                low++;
             }
+         else if(nums[low] + nums[high] > newTarget){
+            high--;
+          }
+          else {
+            set.insert({nums[i], nums[j], nums[low], nums[high]});
+            low++;
+            high--;
+          }
+         }
         }
-        return ans;
-    }
+       }
+        
+       for(auto it : set){
+        output.push_back(it);
+       }
+       return output;
+    }
 };
