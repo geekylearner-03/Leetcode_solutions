5d39f772b3dccafdb753faee118d63fecc0826cd
Sakshi Singhania <122166462+geekylearner-03@users.noreply.github.com>
2025-04-05 22:59:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (41.29%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int left = 0; // Points to the next position to place a non-val element
+        int left = 0; 
         for (int right = 0; right < nums.size(); ++right) {
             if (nums[right] != val) {
                 nums[left] = nums[right];
                 left++;
             }
         }
-        return left; // The first `left` elements are non-val
+        return left;
     }
 };
